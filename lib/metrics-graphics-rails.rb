7f88c0e600ca2fad2a19048e06b0fa3a23c77951
David Gil <dgilperez@gmail.com>
2016-05-05 14:54:19 +0200

    adds missing require

--- lib/metrics-graphics-rails.rb
+++ lib/metrics-graphics-rails.rb
@@ -1,7 +1,7 @@
+require "d3-rails"
 require "metrics-graphics-rails/version"
 require 'metrics-graphics-rails/view_helpers'
 require "metrics-graphics-rails/engine" if defined?(::Rails)
 
 # require 'metrics-graphics-rails/version'
 # require 'metrics-graphics-rails/view_helpers'
-
