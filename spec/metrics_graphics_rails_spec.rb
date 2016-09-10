2c98e53b4fb704ca2f2d181bcf45933b966aeea5
David Gil <dgilperez@gmail.com>
2016-09-10 23:47:01 +0200

    updates metrics-graphics to 2.10.1

--- spec/metrics_graphics_rails_spec.rb
+++ spec/metrics_graphics_rails_spec.rb
@@ -4,46 +4,9 @@ require 'active_support'
 
 include ActionView::Helpers::JavaScriptHelper
 include ActionView::Helpers::TagHelper
-#include ActionView::Context
 include MetricsGraphicsRails::ViewHelpers
 
 describe MetricsGraphicsRails::ViewHelpers, type: :helper do
-  # before do
-  #   allow(self).to receive(:metrics_graphic_for) { :inactive }
-  # end
-
-  #
-  # def metrics_graphic_for(data, options = {})
-  #   @data              = data
-  #   json_data          = data.to_json
-  #   @target            = options.fetch(:target)
-  #   @x_accessor        = options.fetch(:x_accessor)  { :date }
-  #   @y_accessor        = options.fetch(:y_accessor)  { :value }
-  #   title              = options.fetch(:title)
-  #   description        = options.fetch(:description) { '' }
-  #   width              = options.fetch(:width)       { 600 }
-  #   height             = options.fetch(:height)      { 250 }
-  #   @time_format       = options.fetch(:time_format) { '%Y-%m-%d' }
-  #   @is_multiple       = data.first.is_a?(Array)
-  #   @extra_options     = options[:extra_options] || {}
-  #
-  #   javascript_tag <<-SCRIPT
-  #     var data = #{json_data};
-  #     #{convert_data_js}
-  #     MG.data_graphic({
-  #       title: "#{title}",
-  #       description: "#{description}",
-  #       data: data,
-  #       width: #{width},
-  #       height: #{height},
-  #       target: '#{@target}',
-  #       #{extra_options_to_options}
-  #       x_accessor: '#{@x_accessor}',
-  #       y_accessor: '#{@y_accessor}'
-  #     });
-  #   SCRIPT
-  # end
-
   let(:data) { [{ date: '2014-11-01', value: 12 }, { date: '2014-11-02', value: 18 }] }
 
   describe "metrics_graphic_for" do
